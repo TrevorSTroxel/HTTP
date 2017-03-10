9e4a40fccd04255c5e3498d58a5441c63c5b7201
TrevorSTroxel <actroxel@gmail.com>
2017-03-10 11:53:39 -0800

    Add files via upload

---
+++ index (copy).php
@@ -0,0 +1,1 @@
+trevort.ioe
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Delta.ioe</title>
+<link rel="stylesheet" href="css/custom.css">
+<!-- <link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/neon.css"> -->
+<style>
+#if-location {
+position: absolute; left: 10px; top:200px;
+width:800px;
+height 500px;
+}
+img:hover {
+opacity: 0.5;
+}
+</style>
+</head>
+<body>
+<center>
+<!-- add more menu items here -->
+<!--<a href="phpcode/" class = "menu" target = " blank">PHP code</a>&nbsp; -->
+<a href="downloads/" class = "slink" target = " blank">Downloads</a>&nbsp;
+<a href="qnb/" class = "menu" target = " blank">PMA</a>&nbsp;
+<a href="logs/" class = "slink" target = " blank" >Logs</a>
+<a href="dev/" class = "menu" target = " blank">DEV</a>
+<a href="contact.html" class = "slink" target = " blank">Contact</a>
+<a href="http://10.183.2.4:8080" class = "menu" target = " blank">Node Server</a>
+<a href="http://10.183.2.4:3000" class = "slink" target = " blank">Exapp</a>
+<br />
+<a href = "html" target = "_blank"><img src = "images/html_button.png" height = "100"></a> 
+<a href = "" target = "_blank"><img src = "images/word_press_button.png" height = "100"></a> 
+<a href = "php" target = "_blank"><img src = "images/php_app.png" height = "100"></a> 
+<a href = "https://sites.google.com/a/tritechsc.org/trevort/" target = "_blank"><img src = "images/google_site.png" height = "100"></a> 
+</center>
